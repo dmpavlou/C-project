94bdf2016d21ca5d29650e2b79dd9ab45cb4e29b
dmpavlou <demmyp98@gmail.com>
2023-11-21 14:03:56 +0100

    newewr

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(){
     int firstN {3}; // statements always finish with ;
     int secondN {6};
     int thirdN {6};
+    int fourthN {90};
 
     int sum = firstN + secondN;
     std::cout << "The sum of the two numbers is: " << sum << std::endl;
