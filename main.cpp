3be94fe140713cc39b8f1d186074650cc479f526
dmpavlou <demmyp98@gmail.com>
2023-11-21 13:08:19 +0100

    First version of CPP program

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(){
+
+    int firstN {3};
+    int secondN {6};
+
+    int sum = firstN + secondN;
+    std::cout << "The sum of the two numbers is: " << sum << std::endl;
+
+    return 0;
+
+}
