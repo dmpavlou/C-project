a47e360da0d6b974157cd53c1638524a33971eb7
dmpavlou <demmyp98@gmail.com>
2023-11-22 14:33:39 +0100

    Creating functions before the main one, then using them inside it

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
+int adding_numbers(int number1, int number2){
+    int result = number1 + number2;
+    return result;
+    /*
+    This is a function, exactly like the main one. You can recall it inside the main one.
+    VERY convinient, since this way you can have some processes already done and compressed
+    in a singe phrase something(x, y) instead of filling the main one with more things.
+    */
+}
 
 int main(){
 
     int firstN {3}; // statements always finish with ;
     int secondN {6};
-    int thirdN {6};
-    int fourthN {90};
 
-    int sum = firstN + secondN;
+    int sum = adding_numbers(3, 6);  // Ηere we recall the function for adding numbers and store the result in "sum". No need to do the + here!
     std::cout << "The sum of the two numbers is: " << sum << std::endl;
-    std::cout << "But a random third number is: " << thirdN;
 
     return 0;
 
